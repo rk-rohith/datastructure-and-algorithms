bb6e9cb7b0b66b28da357216d762e54582eda033
rk-rohith <rk.rohithkumar97@gmail.com>
2024-08-17 09:56:41 +0530

    Add twoSum problem with description

---
+++ src/main/ds/arrrays/Easy/twosum/twoSum.java
@@ -0,0 +1,29 @@
+package ds.arrrays.Easy.twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class twoSum {
+    public static void main(String[] args) {
+        // Test the solution with the given example
+        int[] nums = {2, 11, 15, 7};
+        int target = 9;  // Expected output: [0, 1]  (since nums[0] + nums[1] = 9)
+
+        int[] result = twoSum(nums, target);
+        System.out.println("Two sum result: " + result[0] + ", " + result[1]); // Output: 0, 1
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diffVal = target - nums[i];
+            if (map.containsKey(diffVal)) {
+                return new int[]{map.get(diffVal), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
